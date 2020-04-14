b172564866ad84f14bcb5bb40062d454cd720808
Kevin Jordan <macdev2@Kevins-MacBook-Pro.local>
2020-04-14 10:49:04 -0700

    store update

--- src/store/store.js
+++ src/store/store.js
@@ -6,8 +6,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
 
     state: {
-        modalState: true,
-        modalType: 'trouble',
+        modalState: false,
+        modalType: '',
         mssgs: [
             'Flexible Telecom Solutions',
             'We Grow With You',

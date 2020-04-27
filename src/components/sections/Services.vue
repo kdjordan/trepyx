2dfd7381f4f95f4c1a2e739475e1af18cd93703e
Kevin Jordan <macdev2@Kevins-MacBook-Pro.local>
2020-04-26 20:12:54 -0700

    fix

--- src/components/sections/Services.vue
+++ src/components/sections/Services.vue
@@ -76,16 +76,16 @@ export default {
 @import '../../scss/utils.scss';
 
 
-.services-card__item:nth-child(even) {
-    & img {
-      transform: rotateX(-45deg);
-    }
-}
-.services-card__item:nth-child(odd) {
-    & img {
-      transform: rotateX(30deg);
-    }
-}
+// .services-card__item:nth-child(even) {
+//     & img {
+//       transform: rotateX(-45deg);
+//     }
+// }
+// .services-card__item:nth-child(odd) {
+//     & img {
+//       transform: rotateX(30deg);
+//     }
+// }
 
 .services {
   height: 23rem;

1f9e700e70beee8551bcf34d50b440e38fcc8d9d
Kevin Jordan <macdev2@Kevins-MacBook-Pro.local>
2020-04-26 19:54:40 -0700

    fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,7 +21,7 @@
       
           <nav class="mobile-navigation__nav">
             <ul class="mobile-navigation__list">
-              <li class="mobile-navigation__items"><a href="#pricing" class="mobile-navigation__link">01&nbsp;&nbsp;Plans & Pricingz</a></li>
+              <li class="mobile-navigation__items"><a href="#pricing" class="mobile-navigation__link">01&nbsp;&nbsp;Plans & Pricing</a></li>
               <li class="mobile-navigation__items"><a href="/quote" class="mobile-navigation__link">02&nbsp;&nbsp;Get a Quote</a></li>
               <li class="mobile-navigation__items"><a href="#contact" class="mobile-navigation__link">03&nbsp;&nbsp;Contact Us</a></li>
               <li class="mobile-navigation__items"><a href="https://login.ringlogix.com" class="mobile-navigation__link">04&nbsp;&nbsp;Login</a></li>
